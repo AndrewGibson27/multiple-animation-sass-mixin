4f93d1858587cfcc63810031c50933679e097b2c
Andrew Gibson <atgibson27@gmail.com>
2017-01-21 15:20:30 -0600

    fix indentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,15 @@
 module.exports = function(grunt){
-
 	grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        
-        sass: {
-            dist: {
-              files: {
-                'example/main.css': 'example/main.scss'
-              }
-            }
-        }
-    });
-    
+		pkg: grunt.file.readJSON('package.json'),
+
+		sass: {
+			dist: {
+				files: {
+					'example/main.css': 'example/main.scss'
+				}
+			}
+		}
+	});
+
 	grunt.loadNpmTasks('grunt-contrib-sass');
 };
